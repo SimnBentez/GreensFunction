c854050ba41e9f527a40e0653c0993ef97d3f669
Simón Benítez Soto <simonbenitez627@gmail.com>
2026-05-03 20:37:17 -0500

    More corrections

--- src/solver.cpp
+++ src/solver.cpp
@@ -8,7 +8,6 @@ Solver::Solver(int N_, double L_, const Material& mat_)
 }
 
 void Solver::computeTheta() {
-
     Coeff FY(0,0,1,0);
     Coeff M(0,0,0,1);
 
@@ -16,52 +15,52 @@ void Solver::computeTheta() {
     theta[1] = (-1.0 / mat.getEI(0)) * M;
 
     theta[2] = (1.0 / (2*mat.getEI(0))) *
-               ((-mat.getEI(1))*theta[1] + FY);
+               (((-1.0) * mat.getEI(1)) * theta[1] + FY); // Corregido: escalar a la izquierda
 
     for (int k=2; k<N; ++k) {
         Coeff sum;
 
-        for (int k1=0; k1<k; ++k1)
-            sum = sum + (k1+1)*theta[k1+1]*mat.getEI(k-k1);
+        for (int k1=0; k1<k; ++k1) {
+            // Corregido: Ponemos los escalares mat e (k1+1) a la izquierda de theta
+            double factor = (double)(k1 + 1) * mat.getEI(k - k1);
+            sum = sum + (factor * theta[k1 + 1]);
+        }
 
-        theta[k+1] = (-1.0/((k+1)*mat.getEI(0))) * sum;
+        theta[k+1] = (-1.0 / ((k+1) * mat.getEI(0))) * sum;
     }
 }
 
 void Solver::computeV() {
-
     Coeff FY(0,0,1,0);
 
     v[0] = {1,0,0,0};
-    v[1] = theta[0] - (1.0/mat.getAsG(0))*FY;
+    v[1] = theta[0] + ((-1.0 / mat.getAsG(0)) * FY); // Corregido: escalar a la izquierda
 
     for (int k=1; k<N; ++k) {
-
-        v[k+1] = (1.0/(k+1)) * theta[k];
+        v[k+1] = (1.0 / (k+1)) * theta[k];
 
         for (int k1=0; k1<k; ++k1) {
+            // Corregido: (k1+1) a la izquierda de v
+            Coeff term = ((double)(k1 + 1) * v[k1 + 1]) + ((-1.0) * theta[k1]);
 
-            Coeff term = (k1+1)*v[k1+1] - theta[k1];
-
+            // Corregido: mat.getAsG a la izquierda de term
             v[k+1] = v[k+1]
-                - (1.0/((k+1)*mat.getAsG(0)))
-                * (term * mat.getAsG(k-k1));
+                - (1.0 / ((k+1) * mat.getAsG(0)))
+                * (mat.getAsG(k-k1) * term);
         }
     }
 }
 
 Coeff Solver::evaluate(const std::vector<Coeff>& coeffs) {
-
     Coeff result;
-
-    for (int k=0; k<=N; ++k)
-        result = result + std::pow(L,k)*coeffs[k];
-
+    for (int k=0; k<=N; ++k) {
+        // Corregido: el resultado de pow (double) a la izquierda de coeffs
+        result = result + (std::pow(L, k) * coeffs[k]);
+    }
     return result;
 }
 
 ShapeFunctions Solver::solveShape(double ui, double ti, double vj, double tj) {
-
     computeTheta();
     computeV();
 
